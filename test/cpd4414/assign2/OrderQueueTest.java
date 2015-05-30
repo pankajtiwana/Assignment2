be6252a6e9ecc780150f56f5943bd41a2c938c7f
pankajtiwana <pankajtiwana@pankajs-mbp>
2015-05-30 15:33:17 -0400

    Checking if program throws an exception if neither customer id or customer name exist. added test WhenNoCustomerException

--- test/cpd4414/assign2/OrderQueueTest.java
+++ test/cpd4414/assign2/OrderQueueTest.java
@@ -54,7 +54,7 @@ public class OrderQueueTest {
     }
 
     @Test
-    public void testWhenCustomerExistsAndPurchasesExistThenTimeReceivedIsNow() {
+    public void testWhenCustomerExistsAndPurchasesExistThenTimeReceivedIsNow() throws Exception {
         OrderQueue orderQueue = new OrderQueue();
         Order order = new Order("CUST00001", "ABC Cafeteria");
         order.addPurchase(new Purchase("PROD0004", 450));
@@ -64,6 +64,23 @@ public class OrderQueueTest {
         long expResult = new Date().getTime();
         long result = order.getTimeReceived().getTime();
         assertTrue(Math.abs(result - expResult) < 1000);
+    }
+    @Test
+    public void WhenNoCustomerException()
+    {
+        boolean catchexception= false;
+         OrderQueue orderQueue = new OrderQueue();
+        Order order = new Order(null , null);
+        order.addPurchase(new Purchase("PROD0004", 450));
+        order.addPurchase(new Purchase("PROD0006", 250));
+        try{
+        orderQueue.add(order);
+        }
+        catch(Exception e)
+        {
+            catchexception=true;
+        }
+        assertTrue(catchexception);
     }
     
 }
